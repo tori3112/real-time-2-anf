8bc9c8d38b7f43aa035241ff0ab3406dfa5b8a82
tori3112 <146270794+tori3112@users.noreply.github.com>
2024-12-28 23:49:14 +0100

    Update anf.c

--- CCS_StartFiles/Assignment_2/anf.c
+++ CCS_StartFiles/Assignment_2/anf.c
@@ -19,7 +19,7 @@ int anf(int y, int *s, int *a, int *rho, unsigned int* index)
     // STEP 1: rho = lambda * rho(k-1)+ rho(inf) * (1-lambda)
     AC0 = ((long)rho[0]) * lambda;                      // Q15 * Q15 = Q30
     AC1 = ((long)rho[1]) * (32768-lambda);              // Q15 * Q15 = Q30
-    AC0 = AC0 + AC1;                                         // Subtraction in Q30
+    AC0 = AC0 + AC1;                                    // Subtraction in Q30
     AC0 = AC0 >> 15;                                    // Scale to Q15
     rho[0] = (int)AC0;                                  // Update rho in Q15
 
@@ -28,39 +28,39 @@ int anf(int y, int *s, int *a, int *rho, unsigned int* index)
 
 
     // STEP 2: s[k] = y + (rho * a * s[k-1]) - (rho^2 * s[k-2])
-	AC0 = ((long)rho[0]) * (*a);                        // Q15 * Q13 = Q28.
-	AC0 = AC0 >> 13;                                    // Temporarily scale to Q15. 28-15 = 13
-	AC0 = AC0 * s[(k + 2) % 3];                         // Q15 * Q12 = Q27
+	AC0 = ((long)rho[0]) * (*a);                    // Q15 * Q13 = Q28.
+	AC0 = AC0 >> 13;                                // Temporarily scale to Q15. 28-15 = 13
+	AC0 = AC0 * s[(k + 2) % 3];                     // Q15 * Q12 = Q27
 
-	AC1 = AC1 * s[(k + 1) % 3];              // Q15 * Q12 = Q27.
-	AC0 = AC0 - AC1;                                    // Subtraction in Q27
+	AC1 = AC1 * s[(k + 1) % 3];              	// Q15 * Q12 = Q27.
+	AC0 = AC0 - AC1;                                // Subtraction in Q27
 
-	AC1 = (long)y << 12;                                // Scale to Q27. 15-27 = -12
-	AC0 = AC0 + AC1;                                    // Addition in Q27
+	AC1 = (long)y << 12;                            // Scale to Q27. 15-27 = -12
+	AC0 = AC0 + AC1;                                // Addition in Q27
 
-	AC0 = AC0 >> 15;                                    // Scale back to Q12. 27-12 = 15
-	s[k] =(int)AC0;                                     // Update s[k] in Q12.
+	AC0 = AC0 >> 15;                                // Scale back to Q12. 27-12 = 15
+	s[k] =(int)AC0;                                 // Update s[k] in Q12.
 
 	//STEP 3: e = s[k] - (a * s[k-1]) + s[k-2]
-	AC1 = ((long)(*a)) * s[(k + 2) % 3];                // Q13 * Q12 = Q25.
-	AC1 = AC1 >> 10;                                    // Scale to Q15. 25-15 = 10
+	AC1 = ((long)(*a)) * s[(k + 2) % 3];            // Q13 * Q12 = Q25.
+	AC1 = AC1 >> 10;                                // Scale to Q15. 25-15 = 10
 
-	AC0 = (long)s[k] << 3;                              // Scale to Q15. 12-15 = -3
-	AC0 = AC0 - AC1;                                    // Subtraction in Q15
+	AC0 = (long)s[k] << 3;                          // Scale to Q15. 12-15 = -3
+	AC0 = AC0 - AC1;                                // Subtraction in Q15
 
-	AC1 = (long)s[(k + 1) % 3] << 3;                    // Scale to Q14. 12-15 = -3
-	AC0 = AC0 + AC1;                                    // Addition in Q15
+	AC1 = (long)s[(k + 1) % 3] << 3;                // Scale to Q14. 12-15 = -3
+	AC0 = AC0 + AC1;                                // Addition in Q15
 
-	e = (int)AC0;                                       // Update e in Q15
+	e = (int)AC0;                                   // Update e in Q15
 
 	//STEP 4: a = a + (2 * mu * s[k-1] * e)
-	AC0 = (long)s[(k + 2) % 3] * e;                     // Q12 * Q15 = Q27
-	AC0 = AC0 >> 12;                                    // Temporarily scale to Q15. 27-15 = 12
-	AC0 =  AC0 * 2 * mu;                                // Q15 * Q15 = Q30
-	AC0 = AC0 >> 17;                                    // Scale to Q13. 30-13 = 17
+	AC0 = (long)s[(k + 2) % 3] * e;                 // Q12 * Q15 = Q27
+	AC0 = AC0 >> 12;                                // Temporarily scale to Q15. 27-15 = 12
+	AC0 =  AC0 * 2 * mu;                            // Q15 * Q15 = Q30
+	AC0 = AC0 >> 17;                                // Scale to Q13. 30-13 = 17
 
-	AC1 = ((long)(*a));                                 // Load previous a
-	AC0 = AC0 + AC1;                                    // Addition in Q13
+	AC1 = ((long)(*a));                             // Load previous a
+	AC0 = AC0 + AC1;                                // Addition in Q13
 
 	// Check stability bounds (|a| < 2 in Q13)
     if (AC0 > (1 << 14) - 1) {
